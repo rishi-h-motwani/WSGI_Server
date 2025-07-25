0e827c1855dec7b4070ff7ed265f08c1d26158a2
Rishi Motwani <rmotwani056@gmail.com>
2025-07-26 05:04:44 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,131 @@
+import io
+import sys
+import socket
+import threading
+
+
+class WGCI_Sev(object):
+    request_queue_size = 5
+    def __init__(self,server_address):  
+        self.server_socket = server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server_socket.bind(server_address)
+        server_socket.listen(self.request_queue_size)
+        host,port = self.server_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        self.headers_set = []
+    def set_app(self,application):
+        self.application = application
+        
+    def serve_forever(self):
+        while True:
+            self.client_connection, _ = self.server_socket.accept()
+            thread = threading.Thread(target=self.handle_one_request)
+            thread.daemon = True
+            thread.start()            
+    
+    def handle_one_request(self):
+        request_data = self.client_connection.recv(1024)
+        self.request_data = request_data = request_data.decode('utf-8')
+        # Print formatted request data a la 'curl -v'
+        print(''.join(
+            f'< {line}\n' for line in request_data.splitlines()
+        ))
+
+        self.parse_request(request_data)
+
+        # Construct environment dictionary using request data
+        env = self.get_environ()
+
+        # It's time to call our application callable and get
+        # back a result that will become HTTP response body
+        result = self.application(env, self.start_response)
+
+        # Construct a response and send it back to the client
+        self.finish_response(result)
+
+    def parse_request(self, text):
+        lines = text.splitlines()
+        if not lines:  # Empty request
+            raise ValueError("Empty HTTP request")
+
+        request_line = lines[0].rstrip('\r\n')
+        parts = request_line.split()
+        if len(parts) != 3:  # Malformed request line
+            raise ValueError(f"Invalid HTTP request line: {request_line}")
+
+        self.request_method, self.path, self.request_version = parts
+
+    def get_environ(self):
+        env = {}
+        # The following code snippet does not follow PEP8 conventions
+        # but it's formatted the way it is for demonstration purposes
+        # to emphasize the required variables and their values
+        #
+        # Required WSGI variables
+        env['wsgi.version']      = (1, 0)
+        env['wsgi.url_scheme']   = 'http'
+        env['wsgi.input']        = io.StringIO(self.request_data)
+        env['wsgi.errors']       = sys.stderr
+        env['wsgi.multithread']  = True
+        env['wsgi.multiprocess'] = False
+        env['wsgi.run_once']     = False
+        # Required CGI variables
+        env['REQUEST_METHOD']    = self.request_method    # GET
+        env['PATH_INFO']         = self.path              # /hello
+        env['SERVER_NAME']       = self.server_name       # localhost
+        env['SERVER_PORT']       = str(self.server_port)  # 8888
+        return env
+    
+    def start_response(self, status, response_headers, exc_info=None):
+        # Add necessary server headers
+        server_headers = [
+            ('Date', 'Mon, 15 Jul 2019 5:54:48 GMT'),
+            ('Server', 'WSGIServer 0.2'),
+        ]
+        self.headers_set = [status, response_headers + server_headers]
+        return self.finish_response    
+
+    def finish_response(self, result):
+        try:
+            status, response_headers = self.headers_set
+            response = f'HTTP/1.1 {status}\r\n'
+            for header in response_headers:
+                response += '{0}: {1}\r\n'.format(*header)
+            response += '\r\n'
+            for data in result:
+                response += data.decode('utf-8')
+            # Print formatted response data a la 'curl -v'
+            print(''.join(
+                f'> {line}\n' for line in response.splitlines()
+            ))
+            response_bytes = response.encode()
+            self.client_connection.sendall(response_bytes)
+        finally:
+            self.client_connection.close()    
+    
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+
+
+def make_server(server_address, application):
+    server = WGCI_Sev(server_address)
+    server.set_app(application)
+    return server
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit('Provide a WSGI application object as module:callable')
+    app_path = sys.argv[1]
+    module, application = app_path.split(':')
+    module = __import__(module)
+    application = getattr(module, application)
+    httpd = make_server(SERVER_ADDRESS, application)
+    print(f'WSGIServer: Serving HTTP on port {PORT} ...\n')
+    httpd.serve_forever()    
+
+
+
+
